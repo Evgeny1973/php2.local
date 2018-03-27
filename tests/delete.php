80c99acf6a72ae66a6ea0170ae6edc91bc5af2b8
Evgeny <gren33@mail.ru>
2018-03-27 18:37:21 +0300

    ДЗ-1

--- tests/delete.php
+++ tests/delete.php
@@ -4,6 +4,6 @@ require __DIR__ . '/../autoload.php';
 $dbh = new \App\Db;
 
 $sql = 'DELETE FROM news WHERE id=:id';
-$res = $dbh->execute($sql, [':id' => 6]);
+$res = $dbh->execute($sql, [':id' => 5]);
 
 var_dump($res);
