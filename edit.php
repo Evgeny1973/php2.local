92c75ff40ba0b7138a0d491a09698f006bd6dfc1
Evgeny <gren33@mail.ru>
2018-04-06 10:59:28 +0300

    ДЗ-3

--- edit.php
+++ edit.php
@@ -1,7 +1,6 @@
 <?php
 include __DIR__ . '/autoload.php';
 
-
-$view->article = \App\Models\Article::findById($_GET['id']);
 $view = new \App\View\View;
+$view->article = \App\Models\Article::findById($_GET['id']);
 $view->display(__DIR__ . '/templates/edit.php');
--- update.php
+++ update.php
@@ -1,13 +1,11 @@
 <?php
+include __DIR__ . '/autoload.php';
+
 if (isset($_POST['submit'])) {
     $article = \App\Models\Article::findById($_POST['id']);
     $article->title = ($_POST['title']);
     $article->content = ($_POST['content']);
     $article->id = ($_POST['id']);
     $article->author_id = ($_POST['author_id']);
-    var_dump($article);
-    die;
-    //$data->save();
     header('Location: /admin.php');
-    exit;
 }
