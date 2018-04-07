02b0d63b0e3e2135f8307f814f10612193729821
Evgeny <gren33@mail.ru>
2018-04-08 00:33:42 +0300

    ДЗ-3

--- article.php
+++ article.php
@@ -2,10 +2,9 @@
 include __DIR__ . '/autoload.php';
 
 if (isset($_GET['id'])) {
-    $article = \App\Models\Article::findById($_GET['id']);
-    if (isset($article)) {
-        $view = new App\View\View;
-        $view->article = $article;
+    $view = new App\View\View;
+    $view->article = \App\Models\Article::findById($_GET['id']);
+    if (!null == $view->article) {
         $view->display(__DIR__ . '/templates/article.php');
         exit;
     } else {
