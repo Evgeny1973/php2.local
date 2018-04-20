50050f759cdf9fe77385ecca3ca4905108a11008
Evgeny <gren33@mail.ru>
2018-04-20 23:58:00 +0300

    ДЗ-4

--- index.php
+++ index.php
@@ -3,10 +3,10 @@ include __DIR__ . '/App/autoload.php';
 
 $uri = $_SERVER['REQUEST_URI'];
 
-$parts = explode('/', trim($uri, '/'));
+$parts = explode('/', $uri);
 
-$ctrl = $parts[0] ? ucfirst($parts[0]) : 'Index';
-$action = $parts[1] ?? 'allNews';
+$ctrl = $parts[1] ? ucfirst($parts[1]) : 'Index';
+$action = $parts[2] ?? 'allNews';
 
 $class = '\App\Controllers\\' . $ctrl;
 
